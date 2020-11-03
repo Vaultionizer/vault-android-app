7993e0adf643bc66bcf50ae1c8be6105b11ca88d
Johannes Quast <johannesquast@outlook.de>
2020-11-03 10:33:21 +0100

    Move hardcoded strings to generic string.xml file

--- app/src/main/java/com/vaultionizer/vaultapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/ui/login/LoginFragment.kt
@@ -112,7 +112,7 @@ class LoginFragment : Fragment() {
     }
 
     private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome) + model.displayName
+        val welcome = "Welcome " + model.displayName
         // TODO : initiate successful logged in experience
         val appContext = context?.applicationContext ?: return
         Toast.makeText(appContext, welcome, Toast.LENGTH_LONG).show()
