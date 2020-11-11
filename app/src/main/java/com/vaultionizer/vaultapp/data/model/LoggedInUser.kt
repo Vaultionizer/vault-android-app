06c306354d618380badf8fea50229beb62c4cc17
Johannes Quast <johannesquast@outlook.de>
2020-11-11 14:24:08 +0100

    Add TODO

--- app/src/main/java/com/vaultionizer/vaultapp/data/model/LoggedInUser.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/data/model/LoggedInUser.kt
@@ -3,6 +3,7 @@ package com.vaultionizer.vaultapp.data.model
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
+// TODO(jatsqi) Remove this later
 data class LoggedInUser(
         val userId: String,
         val displayName: String
