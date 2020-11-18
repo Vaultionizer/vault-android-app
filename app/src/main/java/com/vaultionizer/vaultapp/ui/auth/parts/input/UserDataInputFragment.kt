850af096dca7544f69d6e2e0e093cc7f4f5f13dd
Johannes Quast <johannesquast@outlook.de>
2020-11-18 15:07:33 +0100

    Delegate input to view model

--- app/src/main/java/com/vaultionizer/vaultapp/ui/auth/parts/input/UserDataInputFragment.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/ui/auth/parts/input/UserDataInputFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.vaultionizer.vaultapp.R
+import com.vaultionizer.vaultapp.ui.auth.data.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserDataInputFragment : Fragment() {
+
+    private val authViewModel: AuthViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,17 @@ class UserDataInputFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val usernameEdit = view.findViewById<EditText>(R.id.input_username)
+        val passwordEdit = view.findViewById<EditText>(R.id.input_password)
+
+        usernameEdit.addTextChangedListener {
+            authViewModel.usernameDataChanged(usernameEdit.text.toString())
+        }
+
+        passwordEdit.addTextChangedListener {
+            authViewModel.passwordDataChanged(passwordEdit.text.toString())
+        }
     }
 
 }
