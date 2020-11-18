358bdec09c2d9027241538077ec59944c4f91b7c
Johannes Quast <johannesquast@outlook.de>
2020-11-18 15:03:04 +0100

    Merge repositories

---
+++ app/src/main/java/com/vaultionizer/vaultapp/repository/AuthRepository.kt
@@ -0,0 +1,100 @@
+package com.vaultionizer.vaultapp.repository
+
+import com.google.gson.Gson
+import com.vaultionizer.vaultapp.data.db.dao.LocalUserDao
+import com.vaultionizer.vaultapp.data.db.entity.LocalUser
+import com.vaultionizer.vaultapp.data.model.rest.result.ApiResult
+import com.vaultionizer.vaultapp.data.model.rest.result.ManagedResult
+import com.vaultionizer.vaultapp.data.model.rest.rf.ReferenceFile
+import com.vaultionizer.vaultapp.data.model.rest.user.CreateUserRequest
+import com.vaultionizer.vaultapp.data.model.rest.user.LoggedInUser
+import com.vaultionizer.vaultapp.data.model.rest.user.LoginUserRequest
+import com.vaultionizer.vaultapp.data.model.rest.user.UserAuthPair
+import com.vaultionizer.vaultapp.service.UserService
+import com.vaultionizer.vaultapp.hilt.RestModule
+import com.vaultionizer.vaultapp.util.extension.hashSha512
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
+import javax.inject.Inject
+
+class AuthRepository @Inject constructor(val userService: UserService, val gson: Gson, val localUserDao: LocalUserDao) {
+
+    var user: LoggedInUser? = null
+        private set
+
+    val isLoggedIn: Boolean
+        get() = user != null
+
+    init {
+        user = null
+    }
+
+    suspend fun login(host: String, username: String, password: String): Flow<ManagedResult<LoggedInUser>> {
+        RestModule.host = "https://$host"
+
+        return flow {
+            val response = userService.loginUser(LoginUserRequest(username, password.hashSha512()))
+
+            when(response) {
+                is ApiResult.Success -> {
+                    updateLocalUser(username, host, response.data)
+
+                    emit(ManagedResult.Success(user!!))
+                }
+                is ApiResult.NetworkError -> {
+                    emit(ManagedResult.NetworkError(response.exception))
+                }
+                is ApiResult.Error -> {
+                    emit(ManagedResult.UnknownError(response.exception!!))
+                }
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+    suspend fun register(host: String, username: String, password: String, authKey: String): Flow<ManagedResult<LoggedInUser>> {
+        RestModule.host = "https://$host"
+
+        return flow {
+            val response = userService.createUser(CreateUserRequest(password.hashSha512(), gson.toJson(ReferenceFile.EMPTY_FILE), username))
+
+            when(response) {
+                is ApiResult.Success -> {
+                    updateLocalUser(username, host, response.data)
+
+                    emit(ManagedResult.Success(user!!))
+                }
+                is ApiResult.NetworkError -> {
+                    emit(ManagedResult.NetworkError(response.exception))
+                }
+                is ApiResult.Error -> {
+                    if(response.errorCode == 409) {
+                        emit(ManagedResult.UserError.UsernameAlreadyInUseError)
+                    } else if(response.errorCode == 400) {
+                        emit(ManagedResult.UserError.ValueConstraintsError)
+                    } else {
+                        emit(ManagedResult.UnknownError(response.exception ?: IOException()))
+                    }
+                }
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+    private fun updateLocalUser(username: String, host: String, authPair: UserAuthPair) {
+        if(localUserDao.getUserById(authPair.userID) == null) {
+            localUserDao.createUser(LocalUser(authPair.userID, username, host, System.currentTimeMillis()))
+        }
+
+        val localUser = localUserDao.getUserById(authPair.userID)
+        if(localUser != null) {
+            localUser.lastLogin = System.currentTimeMillis()
+            setLoggedInUser(LoggedInUser(localUser, authPair.sessionKey, authPair.websocketToken))
+        }
+    }
+
+    private fun setLoggedInUser(loggedInUser: LoggedInUser) {
+        this.user = loggedInUser
+    }
+}
