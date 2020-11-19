5631cd5b852c30e37b3e40fa2bae83740b509628
Johannes Quast <johannesquast@outlook.de>
2020-11-19 12:21:31 +0100

    Modify space service and add repository

--- app/src/main/java/com/vaultionizer/vaultapp/service/SpaceService.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/service/SpaceService.kt
@@ -1,22 +1,25 @@
 package com.vaultionizer.vaultapp.service
 
+import com.vaultionizer.vaultapp.data.model.rest.result.ApiResult
 import com.vaultionizer.vaultapp.data.model.rest.space.*
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface SpaceService {
 
-    @POST("space/create")
-    fun createSpace(@Body createSpaceRequest: CreateSpaceRequest): Call<Long>
+    @POST("spaces/create")
+    suspend fun createSpace(@Body createSpaceRequest: CreateSpaceRequest): ApiResult<SpaceAuthPair>
 
-    @POST("space/getall")  //TODO change to getAll later on
-    fun getAll(@Body getAllSpacesRequest: GetAllSpacesRequest): Call<Long>
+    @POST("spaces/getAll")
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    suspend fun getAll(): ApiResult<List<SpaceEntry>>
 
-    @POST("space/join")
-    fun join(@Body joinSpaceRequest: JoinSpaceRequest): Call<Long>
+    @POST("spaces/join")
+    suspend fun join(@Body joinSpaceRequest: JoinSpaceRequest): ApiResult<Nothing>
 
-    @POST("space/key")
-    fun getAuthKey(@Body getAuthKeyRequest: GetAuthKeyRequest): Call<SpaceAuthPair>
+    // @POST("space/key")
+    // fun getAuthKey(@Body getAuthKeyRequest: GetAuthKeyRequest): Call<SpaceAuthPair>
 
 }
