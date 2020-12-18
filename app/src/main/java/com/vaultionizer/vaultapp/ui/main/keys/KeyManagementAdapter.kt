ff573953a02d935d74f3c5bf7e7c3bad5fac9ced
Johannes Quast <johannesquast@outlook.de>
2020-12-19 00:39:56 +0100

    Integrate key UI

--- app/src/main/java/com/vaultionizer/vaultapp/ui/main/keys/KeyManagementAdapter.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/ui/main/keys/KeyManagementAdapter.kt
@@ -10,41 +10,49 @@ import androidx.recyclerview.widget.RecyclerView
 import com.vaultionizer.vaultapp.R
 import com.vaultionizer.vaultapp.data.model.domain.VNSpace
 import com.vaultionizer.vaultapp.util.Constants
+import okhttp3.internal.notify
 
-class KeyManagementAdapter(private val dataSet: List<VNSpace>, private val clickListener: (VNSpace) -> Unit) :
+class KeyManagementAdapter(private val clickListener: (VNSpace) -> Unit) :
     RecyclerView.Adapter<KeyManagementAdapter.ViewHolder>() {
 
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val textView: TextView
-            val deleteButton : ImageButton
+    private var dataSet = mutableListOf<VNSpace>()
 
-            init {
-                // Define click listener for the ViewHolder's View.
-                textView = view.findViewById(R.id.textView5)
-                deleteButton = view.findViewById(R.id.key_delete_button)
-            }
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+        val deleteButton : ImageButton
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.textView5)
+            deleteButton = view.findViewById(R.id.key_delete_button)
         }
+    }
 
-        // Create new views (invoked by the layout manager)
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-            // Create a new view, which defines the UI of the list item
-            val view = LayoutInflater.from(viewGroup.context)
-                .inflate(R.layout.fragment_key, viewGroup, false)
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.fragment_key, viewGroup, false)
 
-            return ViewHolder(view)
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = "Keyname: ${Constants.VN_KEY_PREFIX}${dataSet[position].name?:dataSet[position].id}"
+        viewHolder.deleteButton.setOnClickListener{
+            clickListener(dataSet[position])
         }
+    }
 
-        // Replace the contents of a view (invoked by the layout manager)
-        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
 
-            // Get element from your dataset at this position and replace the
-            // contents of the view with that element
-            viewHolder.textView.text = "Keyname: ${Constants.VN_KEY_PREFIX}${dataSet[position].name?:dataSet[position].id}"
-            viewHolder.deleteButton.setOnClickListener{
-                clickListener(dataSet[position])
-            }
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
-        override fun getItemCount() = dataSet.size
+    fun updateSpaces(spaceList: MutableList<VNSpace>) {
+        this.dataSet = spaceList
+        notifyDataSetChanged()
+    }
 }
