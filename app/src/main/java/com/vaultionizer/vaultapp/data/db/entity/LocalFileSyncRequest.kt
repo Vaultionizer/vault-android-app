110717c4bbc0d08e64e10d5235cd6cd6b17fe670
Johannes Quast <johannesquast@outlook.de>
2021-04-10 18:28:49 +0200

    Add and expose basic file sync request entity classes.

---
+++ app/src/main/java/com/vaultionizer/vaultapp/data/db/entity/LocalFileSyncRequest.kt
@@ -0,0 +1,47 @@
+package com.vaultionizer.vaultapp.data.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.net.URI
+
+@Entity
+data class LocalFileSyncRequest(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "request_id")
+    val requestId: Long,
+
+    @ColumnInfo(name = "type")
+    val type: Type,
+
+    @ColumnInfo(name = "user_id")
+    val userId: Long,
+
+    @ColumnInfo(name = "space_id")
+    val spaceId: Long,
+
+    /**
+     * URI to the file in the local file system.
+     * Only present if [type] is [Type.UPLOAD].
+     */
+    @ColumnInfo(name = "uri")
+    val uri: String?,
+
+    /**
+     * Remote ID of the file.
+     *
+     * If [type] is [Type.DOWNLOAD], then this id must be present.
+     *
+     * If [type] is [Type.UPLOAD] and this id is present, then the application already requested
+     * an file id but got interrupted during the upload process. Otherwise the upload has to be
+     * requested.
+     */
+    @ColumnInfo(name = "remote_file_id")
+    val remoteFileId: Long?
+) {
+
+    enum class Type(val id: Int) {
+        DOWNLOAD(0),
+        UPLOAD(1)
+    }
+}
