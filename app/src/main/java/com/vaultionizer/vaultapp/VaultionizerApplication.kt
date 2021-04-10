8798c31103bb511cb7c73298dc3352c75582c74c
Johannes Quast <johannesquast@outlook.de>
2021-04-10 18:00:54 +0200

    Add basic configuration for WorkManager.

--- app/src/main/java/com/vaultionizer/vaultapp/VaultionizerApplication.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/VaultionizerApplication.kt
@@ -1,7 +1,15 @@
 package com.vaultionizer.vaultapp
 
 import android.app.Application
+import androidx.work.Configuration
+import com.vaultionizer.vaultapp.util.Constants
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class VaultionizerApplication : Application()
+class VaultionizerApplication : Application(), Configuration.Provider {
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.INFO)
+            .setMaxSchedulerLimit(Constants.WORK_MANAGER_SCHEDULER_LIMIT)
+            .build()
+}
--- app/src/main/java/com/vaultionizer/vaultapp/util/Constants.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/util/Constants.kt
@@ -2,13 +2,30 @@ package com.vaultionizer.vaultapp.util
 
 object Constants {
 
+    /**
+     * Application
+     */
+    const val WORK_MANAGER_SCHEDULER_LIMIT = 3
+
+    /**
+     * Meta
+     */
     const val MIN_SERVER_VERSION = "0.1"
     const val SESSION_TOKEN_LIFETIME = 30 * 60 * 1000
 
+    /**
+     * Internal File System
+     */
     const val VN_FILE_SUFFIX = "evn"
 
+    /**
+     * Logging
+     */
     const val VN_TAG = "Vault"
 
+    /**
+     * Crypto
+     */
     const val VN_KEY_PREFIX = "vaultionizer_"
     const val VN_KEYSTORE_PROVIDER = "AndroidKeyStore"
     const val VN_KEY_TRANSFER_SIZE = 256
