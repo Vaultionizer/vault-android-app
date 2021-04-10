d44d1a2fdcd8a569db2f1e8f0c530cb5278599f8
Johannes Quast <johannesquast@outlook.de>
2021-04-10 21:16:43 +0200

    Introduce Update Method for remote file id.

--- app/src/main/java/com/vaultionizer/vaultapp/data/db/dao/LocalFileSyncRequestDao.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/data/db/dao/LocalFileSyncRequestDao.kt
@@ -3,6 +3,7 @@ package com.vaultionizer.vaultapp.data.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.vaultionizer.vaultapp.data.db.entity.LocalFileSyncRequest
 
 @Dao
@@ -16,5 +17,8 @@ interface LocalFileSyncRequestDao {
 
     @Insert
     fun createRequest(request: LocalFileSyncRequest): Long
+
+    @Query("UPDATE LocalFileSyncRequest SET remote_file_id = :remoteFileId WHERE request_id = :requestId")
+    fun updateRemoteFileId(requestId: Long, remoteFileId: Long)
 
 }
