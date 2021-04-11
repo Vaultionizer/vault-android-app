974fe5cc3cf25b76762a41ed440d5a238b6110af
Johannes Quast <johannesquast@outlook.de>
2021-04-11 13:51:17 +0200

    Add worker support for Hilt.

--- app/src/main/java/com/vaultionizer/vaultapp/VaultionizerApplication.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/VaultionizerApplication.kt
@@ -1,15 +1,21 @@
 package com.vaultionizer.vaultapp
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import com.vaultionizer.vaultapp.util.Constants
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class VaultionizerApplication : Application(), Configuration.Provider {
+
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
     override fun getWorkManagerConfiguration(): Configuration =
         Configuration.Builder()
             .setMinimumLoggingLevel(android.util.Log.INFO)
             .setMaxSchedulerLimit(Constants.WORK_MANAGER_SCHEDULER_LIMIT)
+            .setWorkerFactory(workerFactory)
             .build()
 }
