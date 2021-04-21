07258ca56fca2624bc3c5a23eb06d7040dcddd2b
Johannes Quast <johannesquast@outlook.de>
2021-04-21 21:20:05 +0200

    Force json as content type.

--- app/src/main/java/com/vaultionizer/vaultapp/service/FileService.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/service/FileService.kt
@@ -5,6 +5,7 @@ import com.vaultionizer.vaultapp.data.model.rest.request.DownloadFileRequest
 import com.vaultionizer.vaultapp.data.model.rest.request.UploadFileRequest
 import com.vaultionizer.vaultapp.data.model.rest.result.ApiResult
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
 
@@ -14,6 +15,7 @@ interface FileService {
     suspend fun uploadFile(@Body uploadFileReq: UploadFileRequest): ApiResult<Long>
 
     @PUT("api/file/download")
+    @Headers("Content-Type: application/json")
     suspend fun downloadFile(@Body downloadFileReq: DownloadFileRequest)
 
     @PUT("api/file/delete")                                                 //TODO will be changed to delete in the future 11/26/2020
