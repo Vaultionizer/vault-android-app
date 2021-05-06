223aa8aa082c999e3ed3d5c94ce3432ffd13be7c
Johannes Quast <johannesquast@outlook.de>
2021-05-06 15:03:41 +0200

    Add handlers for LiveData changes.

--- app/src/main/java/com/vaultionizer/vaultapp/ui/main/status/FileStatusFragment.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/ui/main/status/FileStatusFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vaultionizer.vaultapp.R
 import com.vaultionizer.vaultapp.ui.viewmodel.FileStatusViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,18 +16,30 @@ import dagger.hilt.android.AndroidEntryPoint
 class FileStatusFragment : Fragment() {
 
     private val statusViewModel: FileStatusViewModel by activityViewModels()
+    private lateinit var statusAdapter: FileStatusListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_file_status, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val fileStatusRecyclerView: RecyclerView = view.findViewById(R.id.list_file_status)
+        statusAdapter = FileStatusListAdapter()
 
+        fileStatusRecyclerView.layoutManager = LinearLayoutManager(context)
+        fileStatusRecyclerView.adapter = statusAdapter
+
+        statusViewModel.workInfo.observe(viewLifecycleOwner) {
+            statusViewModel.onWorkerStatusChange(it)
+        }
+
+        statusViewModel.fileStatus.observe(viewLifecycleOwner) {
+            statusAdapter.fileStatusChange(it)
+        }
     }
 }
