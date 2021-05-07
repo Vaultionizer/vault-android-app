d8a6eaf4161ca6587b9c2eab785d1753cbc10c18
Johannes Quast <johannesquast@outlook.de>
2021-05-07 18:10:45 +0200

    Add in-memory cache to store decryption results.

---
+++ app/src/main/java/com/vaultionizer/vaultapp/data/cache/DecryptionResultCache.kt
@@ -0,0 +1,25 @@
+package com.vaultionizer.vaultapp.data.cache
+
+import androidx.lifecycle.MutableLiveData
+import com.vaultionizer.vaultapp.data.model.domain.VNFile
+import okhttp3.internal.EMPTY_BYTE_ARRAY
+
+class DecryptionResultCache {
+
+    private val fileDataPairs = mutableSetOf<DecryptionFileDataPair>()
+
+    private val decryptionResults_ = MutableLiveData<Set<DecryptionFileDataPair>>()
+    val decryptionResultsLiveData = decryptionResults_
+
+    fun addDecryptionResult(file: VNFile, data: ByteArray) {
+        fileDataPairs.add(DecryptionFileDataPair(file, data))
+        decryptionResultsLiveData.postValue(fileDataPairs)
+    }
+
+    fun invalidateResult(file: VNFile) {
+        if (fileDataPairs.remove(DecryptionFileDataPair(file, EMPTY_BYTE_ARRAY))) {
+            decryptionResults_.postValue(fileDataPairs)
+        }
+    }
+
+}
