e30f66ce39fbd56d99a174be561fb47680ef6a25
Johannes Quast <johannesquast@outlook.de>
2021-05-12 18:05:59 +0200

    Add fileAlertType as optional property to display dialog.

--- app/src/main/java/com/vaultionizer/vaultapp/ui/main/file/FileDialogState.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/ui/main/file/FileDialogState.kt
@@ -2,5 +2,6 @@ package com.vaultionizer.vaultapp.ui.main.file
 
 data class FileDialogState(
     val fileError: Int? = null,
+    val fileAlertType: FileAlertDialogType? = null,
     val isValid: Boolean = false
 )
