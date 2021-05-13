2612c9fb9895e4d708cb71a67aa3672f0d7df63d
Johannes Quast <johannesquast@outlook.de>
2021-05-13 18:14:15 +0200

    Integrate NetworkBoundResource into MiscRepository.

--- app/src/main/java/com/vaultionizer/vaultapp/repository/impl/MiscRepositoryImpl.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/repository/impl/MiscRepositoryImpl.kt
@@ -1,15 +1,12 @@
 package com.vaultionizer.vaultapp.repository.impl
 
-import android.util.Log
 import com.vaultionizer.vaultapp.data.model.rest.misc.NetworkVersion
 import com.vaultionizer.vaultapp.data.model.rest.result.ApiResult
+import com.vaultionizer.vaultapp.data.model.rest.result.NetworkBoundResource
 import com.vaultionizer.vaultapp.data.model.rest.result.Resource
 import com.vaultionizer.vaultapp.repository.MiscRepository
 import com.vaultionizer.vaultapp.service.MiscService
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import javax.inject.Inject
@@ -17,24 +14,26 @@ import javax.inject.Inject
 class MiscRepositoryImpl @Inject constructor(val retrofit: Retrofit) : MiscRepository {
 
     override suspend fun pingHost(host: String): Flow<Resource<NetworkVersion>> {
+        return object : NetworkBoundResource<NetworkVersion, NetworkVersion>() {
+            override fun shouldFetch(): Boolean = true
 
-        return flow {
-            val temp =
-                retrofit.newBuilder().baseUrl("https://$host/").client(OkHttpClient()).build()
-            Log.e("Vault", temp.baseUrl().toString())
-
-            val response = temp.create(MiscService::class.java).getVersionInfo()
-
-            when (response::class) {
-                ApiResult.NetworkError::class -> emit(Resource.NetworkError((response as ApiResult.NetworkError).exception))
-                ApiResult.Error::class -> emit(
-                    Resource.MiscError.HostServerError(
-                        statusCode = (response as ApiResult.Error).statusCode
-                    )
-                )
-                else -> emit(Resource.Success((response as ApiResult.Success).data))
+            override suspend fun fromDb(): Resource<NetworkVersion> {
+                throw RuntimeException("Not reachable.")
             }
-        }.flowOn(Dispatchers.IO)
+
+            override suspend fun saveToDb(networkResult: NetworkVersion) {}
+
+            override suspend fun fromNetwork(): ApiResult<NetworkVersion> {
+                val tempClient =
+                    retrofit.newBuilder().baseUrl("https://$host/").client(OkHttpClient()).build()
+                return tempClient.create(MiscService::class.java).getVersionInfo()
+            }
+
+            override fun dispatchError(result: ApiResult.Error): Resource<NetworkVersion> {
+                return Resource.MiscError.HostServerError(result.statusCode)
+            }
+
+        }.asFlow()
     }
 
 }
