04c6534fb9fe8a22d34af403ea534b91a3a2441e
Johannes Quast <johannesquast@outlook.de>
2021-05-20 09:23:38 +0200

    Add documentation for AuthService.

--- app/src/main/java/com/vaultionizer/vaultapp/repository/AuthRepository.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/repository/AuthRepository.kt
@@ -7,12 +7,38 @@ import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
 
+    /**
+     * Logs in a user on the remote server and stores the returned tokens in the injected singleton
+     * instance of [com.vaultionizer.vaultapp.data.cache.AuthCache].
+     *
+     * @see LoggedInUser
+     * @see LocalUser
+     *
+     * @param host      The hostname of the remote server.
+     * @param username  The username.
+     * @param password  The raw password.
+     * @return          An instance of [LoggedInUser] that represents the logged in user.
+     */
     suspend fun login(
         host: String,
         username: String,
         password: String
     ): Flow<Resource<LoggedInUser>>
 
+    /**
+     * Creates a new user on the remote server and creates an entry for that user in the local
+     * database.
+     *
+     * @see LoggedInUser
+     * @see LocalUser
+     *
+     * @param host      The hostname of the remote server.
+     * @param username  The username.
+     * @param password  The raw password.
+     * @param authKey   The auth-key provided by the system administrator of [host].
+     * @return          An instance of [LoggedInUser] that represents the newly created and already
+     *                  logged in user.
+     */
     suspend fun register(
         host: String,
         username: String,
@@ -20,7 +46,18 @@ interface AuthRepository {
         authKey: String
     ): Flow<Resource<LoggedInUser>>
 
+    /**
+     * Logs out the current user.
+     *
+     * @return True if the logout was successful, false otherwise.
+     */
     suspend fun logout(): Boolean
 
+    /**
+     * Deletes the current user from the remote server and all information related to the user from
+     * the local database.
+     *
+     * @return True if the deletion was successful, false otherwise.
+     */
     suspend fun deleteUser(): Boolean
 }
