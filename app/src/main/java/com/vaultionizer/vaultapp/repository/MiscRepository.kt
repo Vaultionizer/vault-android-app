bac7188bf3cb59e16f42964738c0fd0a8e09de86
Johannes Quast <johannesquast@outlook.de>
2021-05-20 09:37:02 +0200

    Add documentation for MiscRepository.

--- app/src/main/java/com/vaultionizer/vaultapp/repository/MiscRepository.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/repository/MiscRepository.kt
@@ -5,5 +5,15 @@ import com.vaultionizer.vaultapp.data.model.rest.result.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface MiscRepository {
+
+    /**
+     * Pings a host and returns information about the maintainer.
+     *
+     * @see [com.vaultionizer.vaultapp.service.MiscService.getVersionInfo]
+     * @see [com.vaultionizer.vaultapp.data.model.rest.result.NetworkBoundResource]
+     *
+     * @param host  Hostname.
+     * @return      A flow as described for [com.vaultionizer.vaultapp.data.model.rest.result.NetworkBoundResource].
+     */
     suspend fun pingHost(host: String): Flow<Resource<NetworkVersion>>
 }
