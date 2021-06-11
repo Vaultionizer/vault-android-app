db78b277c8939a45a3c5a2f2f64da3512464fdae
Johannes Quast <johannesquast@outlook.de>
2021-06-11 14:42:19 +0200

    Add UI to edit text files.

--- app/src/main/java/com/vaultionizer/vaultapp/ui/main/file/viewer/TextFileViewerFragment.kt
+++ app/src/main/java/com/vaultionizer/vaultapp/ui/main/file/viewer/TextFileViewerFragment.kt
@@ -1,13 +1,18 @@
 package com.vaultionizer.vaultapp.ui.main.file.viewer
 
+import android.graphics.Color
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.library.googlematerial.OutlinedGoogleMaterial
+import com.mikepenz.iconics.utils.colorInt
 import com.vaultionizer.vaultapp.R
 import com.vaultionizer.vaultapp.data.cache.DecryptionResultCache
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +23,8 @@ import javax.inject.Inject
 class TextFileViewerFragment : Fragment() {
 
     private val args: TextFileViewerFragmentArgs by navArgs()
+    private lateinit var editToggleButton: FloatingActionButton
+    private lateinit var textEdit: EditText
 
     @Inject
     lateinit var decryptionResultCache: DecryptionResultCache
@@ -32,16 +39,42 @@ class TextFileViewerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textViewer = view.findViewById<TextView>(R.id.viewer_text)
+
+        textEdit = view.findViewById(R.id.viewer_edit)
+        editToggleButton = view.findViewById(R.id.button_edit_toggle)
 
         val text = String(
             decryptionResultCache.getResultByFileId(args.fileArgs.fileId)
                 ?: getString(R.string.text_file_viewer_error).toByteArray(), Charsets.UTF_8
         )
-        textViewer.movementMethod = ScrollingMovementMethod()
-        textViewer.text = text
+
+        textEdit.movementMethod = ScrollingMovementMethod()
+        textEdit.setText(text)
+        updateEditCapabilities(false)
+        updateEditToggleButtonIcon()
+
+        editToggleButton.setOnClickListener {
+            updateEditCapabilities(!textEdit.isFocusable)
+            updateEditToggleButtonIcon()
+        }
 
         decryptionResultCache.invalidateResultByFileId(args.fileArgs.fileId)
     }
 
+    private fun updateEditToggleButtonIcon() {
+        val icon = if (textEdit.isFocusable)
+            OutlinedGoogleMaterial.Icon.gmo_save
+        else
+            OutlinedGoogleMaterial.Icon.gmo_edit
+
+        editToggleButton.imageTintList = null
+        editToggleButton.setImageDrawable(IconicsDrawable(requireContext(), icon).apply {
+            colorInt = Color.WHITE
+        })
+    }
+
+    private fun updateEditCapabilities(allowEditing: Boolean) {
+        textEdit.isFocusable = allowEditing
+        textEdit.clearFocus()
+    }
 }
